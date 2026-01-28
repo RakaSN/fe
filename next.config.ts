f58f45a1260080a7c15586cce3bd1506f959f878
RakaSN <rakasn9@gmail.com>
2026-01-28 20:07:50 +0700

    Fix: berita

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Izin untuk Unsplash
+        port: '',
+        pathname: '/**',
+      },
+      // Kamu bisa tambah domain lain disini nanti (misal: drive.google.com, dll)
+    ],
+  },
 };
 
 export default nextConfig;
